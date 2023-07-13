49d536d96d515a143dee02adaef6ae5523c96adb
Greenfield01 <greenfieldadolphus22@gmail.com>
2023-07-13 23:17:31 +0100

    test

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,5 +1,5 @@
 export default function getSanFranciscoDescription () {
-  const year = 2017
+  const year = 2017;
   const budget = {
     income: '$119,868',
     gdp: '$154.2 billion',
@@ -8,5 +8,5 @@ export default function getSanFranciscoDescription () {
 
   return `As of ${year}, it was the seventh-highest income county in the United States\
   , with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
-  proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`
+  proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
